eb700e7452d75432d7558e0cfb7d57af11c1658a
Cody Brocious <cody.brocious@gmail.com>
2017-01-23 22:44:49 -0700

    Revert "Added support for subfile refreshing via hash parse."
    This reverts commit 230ed9d044b65f7de25a7c6c7d71c764bf83156f.

--- static/main.js
+++ static/main.js
@@ -120,10 +120,5 @@ window.onpopstate = function(evt) {
 		$('#subfileselector').hide();
 		$('#viewer').hide();
 		selectRootFile(evt.state.fn, true);
-	} else if(evt.state.type == 'sub') {
-		$('#tlfileselector').hide();
-		$('#subfileselector').hide();
-		$('#viewer').hide();
-		selectSubFile(evt.state.fn, true);
 	}
 };
